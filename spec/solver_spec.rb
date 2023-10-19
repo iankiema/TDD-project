f3b55d148e0198a17d2b5653e69361ec023d63b7
iankiema <kiemaian.ik@gmail.com>
2023-10-19 09:43:38 +0300

    Added the test code for factorial code

---
+++ spec/solver_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../solver'
+
+RSpec.describe Solver do
+  describe '#factorial' do
+    it 'returns 1 for factorial of 0' do
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the correct factorial for a positive integer' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
+    end
+
+    it 'raises an exception for a negative integer' do
+      solver = Solver.new
+      expect { solver.factorial(-5) }.to raise_error(ArgumentError)
+    end
+  end
+end
