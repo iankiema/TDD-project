73a604dee353c8da98922f34193b9bcb0c98eb6b
Mustakim <m.m.kiron@gmail.com>
2023-10-19 13:53:25 +0600

    Added the fizzbuzz test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -24,4 +24,35 @@ RSpec.describe Solver do
       expect(solver.reverse('hello')).to eq('olleh')
     end
   end
+
+  describe '#reverse' do
+    it 'returns the reversed string' do
+      solver = Solver.new
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for a number divisible by 3' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" for a number divisible by 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" for a number divisible by both 3 and 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns the number as a string for other cases' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
+
+
